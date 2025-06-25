1116960b8f788d1375d52b218545166092a5041b
julie <itsjuliestromberg@gmail.com>
2025-06-24 23:10:03 -0700

    first commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+SMODS.Atlas {
+    key = "centers",
+    path = "Enhancers.png",
+    raw_key = true,
+
+    px = 71,
+    py = 95
+}
+
+SMODS.Atlas {
+    key = "modicon",
+    path = "icon.png",
+    px = 32,
+    py = 32
+}
